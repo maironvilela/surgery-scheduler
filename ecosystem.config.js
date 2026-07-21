6a06d83d59359d88bdf94cfdfa96aefc2040c5c0
Mairon Vilela <maironvilela@hotmail.com>
2026-07-21 15:38:41 -0300

    config: altera porta padrão da aplicação para 4000 (package.json, ecosystem.config.js e .env)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         {
             name: 'surgery-scheduler',
             script: 'node_modules/next/dist/bin/next',
-            args: 'start -p 3000', // Porta onde o app vai rodar
+            args: 'start -p 4000', // Porta onde o app vai rodar
             instances: '2',      // Usa todos os núcleos da CPU (modo cluster)
             exec_mode: 'cluster',
             env: {
